2becbcfc5427ce52208daf6f2c6d1c8ddf542c1e
mike <memcache@qq.com>
2017-04-28 17:43:04 +0800

    1fix bug

--- vendor/laravel/framework/src/Illuminate/Routing/Router.php
+++ vendor/laravel/framework/src/Illuminate/Routing/Router.php
@@ -999,14 +999,14 @@ class Router implements RegistrarContract, BindingRegistrar
         $this->post('logout', 'Auth\LoginController@logout')->name('logout');
 
         // Registration Routes...
-        $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-        $this->post('register', 'Auth\RegisterController@register');
+    //    $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    //    $this->post('register', 'Auth\RegisterController@register');
 
         // Password Reset Routes...
-        $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-        $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-        $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-        $this->post('password/reset', 'Auth\ResetPasswordController@reset');
+     //   $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+     //   $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+     //   $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+     //   $this->post('password/reset', 'Auth\ResetPasswordController@reset');
     }
 
     /**
