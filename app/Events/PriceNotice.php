0031799f9cd4a7125db03dda1429adb0476176bd
mike <memcache@qq.com>
2017-05-04 16:17:31 +0800

    1.报警幅度修改

--- app/Events/PriceNotice.php
+++ app/Events/PriceNotice.php
@@ -14,7 +14,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 class PriceNotice extends Event implements ShouldBroadcast
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable, InteractsWithSockets;
 
     public $notice;
 
